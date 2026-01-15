f32e23f0a4edd59fe6e11ea5790d544b3fa9f24b
WebisBrian <brian.t.github@gmail.com>
2026-01-15 16:22:32 +0100

    Feat: add Transaction repository

---
+++ src/main/java/com/paymybuddy/infrastructure/repository/TransactionRepository.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.infrastructure.repository;
+
+import com.paymybuddy.domain.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    Page<Transaction> findBySenderAccount_IdOrderByDateDesc(Long accountId, Pageable pageable);
+
+    Page<Transaction> findByReceiverAccount_IdOrderByDateDesc(Long accountId, Pageable pageable);
+}
