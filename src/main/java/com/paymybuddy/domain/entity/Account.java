8775ccd201e00486b026b288e745b31f528825e3
WebisBrian <brian.t.github@gmail.com>
2026-01-15 14:09:52 +0100

    Feat: add Account entity

---
+++ src/main/java/com/paymybuddy/domain/entity/Account.java
@@ -0,0 +1,56 @@
+package com.paymybuddy.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = "user")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision=19, scale=2)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    // private constructor
+    private Account(User user) {
+        this.user = user;
+    }
+
+    // factory method
+    public static Account create(User user) {
+        return new Account(user);
+    }
+
+    // methods
+    public void deposit(BigDecimal amount) {
+        requirePositive(amount);
+        this.balance = this.balance.add(amount);
+    }
+
+    public void withdraw(BigDecimal amount) {
+        requirePositive(amount);
+        this.balance = this.balance.subtract(amount);
+    }
+
+    // utils
+    private static void requirePositive(BigDecimal amount) {
+        if (amount == null || amount.signum() <= 0) {
+            throw new IllegalArgumentException("Le montant indiqué doit être positif.");
+        }
+    }
+}
