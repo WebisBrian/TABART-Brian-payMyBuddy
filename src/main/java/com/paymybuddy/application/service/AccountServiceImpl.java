1728ec34fa1a8525641b6a5542858a585807ea28
WebisBrian <brian.t.github@gmail.com>
2026-01-19 21:53:30 +0100

    Feat: add AccountService class & minimal implementation. Next step -> TDD

--- src/main/java/com/paymybuddy/application/service/AccountServiceImpl.java
+++ src/main/java/com/paymybuddy/application/service/AccountServiceImpl.java
@@ -1,17 +1,24 @@
 package com.paymybuddy.application.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.math.BigDecimal;
 
+@Service
 public class AccountServiceImpl implements AccountService {
 
+    @Transactional(readOnly = true)
     public BigDecimal getBalance(Long userId) {
         return BigDecimal.ZERO;
     }
 
+    @Transactional
     public void deposit(Long userId, BigDecimal amount) {
 
     }
 
+    @Transactional
     public void withdraw(Long userId, BigDecimal amount) {
 
     }
