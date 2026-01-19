b55f214e41733c1ee1712112eb376f5a2a930f5b
WebisBrian <brian.t.github@gmail.com>
2026-01-19 14:53:39 +0100

    Feat: implements addContact() method - actually green tests

--- src/main/java/com/paymybuddy/application/service/UserServiceImpl.java
+++ src/main/java/com/paymybuddy/application/service/UserServiceImpl.java
@@ -1,13 +1,44 @@
 package com.paymybuddy.application.service;
 
 import com.paymybuddy.domain.entity.User;
+import com.paymybuddy.domain.entity.UserContact;
+import com.paymybuddy.infrastructure.repository.UserContactRepository;
+import com.paymybuddy.infrastructure.repository.UserRepository;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    public void addContact(Long userId, Long contactId) {
+    private final UserRepository userRepository;
+    private final UserContactRepository userContactRepository;
 
+    public UserServiceImpl(UserRepository userRepository, UserContactRepository userContactRepository) {
+        this.userRepository = userRepository;
+        this.userContactRepository = userContactRepository;
+    }
+
+    @Override
+    public void addContact(Long userId, Long contactId) {
+        if (userId == null || contactId == null) {
+            throw new IllegalArgumentException("User and contact IDs must not be null.");
+        }
+
+        if (userId.equals(contactId)) {
+            throw new IllegalArgumentException("Cannot add self as contact");
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found."));
+
+        User contact = userRepository.findById(contactId)
+                .orElseThrow(() -> new IllegalArgumentException("Contact not found."));
+
+        if (userContactRepository.existsByUser_IdAndContact_Id(userId, contactId)) {
+            throw new IllegalArgumentException("User already has this contact.");
+        }
+
+        UserContact newContact = UserContact.create(user, contact);
+        userContactRepository.save(newContact);
     }
 
     public void removeContact(Long userId, Long contactId) {
