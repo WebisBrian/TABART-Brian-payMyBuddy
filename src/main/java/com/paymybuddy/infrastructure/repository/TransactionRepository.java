e83e530203731dc8dff48b3853438cea7eb622a8
WebisBrian <brian.t.github@gmail.com>
2026-01-19 11:43:49 +0100

    Feat: add @Query findTransactionHistory

--- src/main/java/com/paymybuddy/infrastructure/repository/TransactionRepository.java
+++ src/main/java/com/paymybuddy/infrastructure/repository/TransactionRepository.java
@@ -4,6 +4,8 @@ import com.paymybuddy.domain.entity.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
@@ -11,5 +13,20 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 
     Page<Transaction> findByReceiverAccount_IdOrderByDateDesc(Long accountId, Pageable pageable);
 
-    Page<Transaction> findTransactionHistory(Long accountId, Pageable pageable);
+    @Query(
+            value = """
+            select t
+            from Transaction t
+            where t.senderAccount.id = :accountId
+               or t.receiverAccount.id = :accountId
+            order by t.date desc
+        """,
+            countQuery = """
+            select count(t)
+            from Transaction t
+            where t.senderAccount.id = :accountId
+               or t.receiverAccount.id = :accountId
+        """
+    )
+    Page<Transaction> findTransactionHistory(@Param("accountId") Long accountId, Pageable pageable);
 }
