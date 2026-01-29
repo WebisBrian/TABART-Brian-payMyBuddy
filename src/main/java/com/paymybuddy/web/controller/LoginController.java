7a924cdf94c29a2696814d35a5117e3b83beb41b
WebisBrian <brian.t.github@gmail.com>
2026-01-29 22:24:33 +0100

    Feat: add LoginController - TDD

--- src/main/java/com/paymybuddy/web/controller/LoginController.java
+++ src/main/java/com/paymybuddy/web/controller/LoginController.java
@@ -1,7 +1,14 @@
 package com.paymybuddy.web.controller;
 
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
 }
