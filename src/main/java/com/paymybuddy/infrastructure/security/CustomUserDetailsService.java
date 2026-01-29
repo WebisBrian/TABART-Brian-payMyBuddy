ef269c4176f61eb0f06fbb0efb3e2ae09874c277
WebisBrian <brian.t.github@gmail.com>
2026-01-29 11:59:53 +0100

    Feat: implements CustomUserDetailsService with TDD - actually green tests

--- src/main/java/com/paymybuddy/infrastructure/security/CustomUserDetailsService.java
+++ src/main/java/com/paymybuddy/infrastructure/security/CustomUserDetailsService.java
@@ -1,10 +1,31 @@
 package com.paymybuddy.infrastructure.security;
 
+import com.paymybuddy.domain.entity.User;
+import com.paymybuddy.infrastructure.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class CustomUserDetailsService {
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
 
-    UserDetails loadUserByUsername(String email) {
-        return null;
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Method calls during authentication, "username" here is the email address
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
     }
 }
