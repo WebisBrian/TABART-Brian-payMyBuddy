db1bf97cbfd4cbf3cf82ec7a145d5f969217b09d
WebisBrian <brian.t.github@gmail.com>
2026-01-29 22:23:58 +0100

    Fix: replace get httpRequest by post

--- src/test/java/com/paymybuddy/web/controller/LoginControllerTest.java
+++ src/test/java/com/paymybuddy/web/controller/LoginControllerTest.java
@@ -3,12 +3,13 @@ package com.paymybuddy.web.controller;
 import com.paymybuddy.infrastructure.security.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LoginController.class)
@@ -27,7 +28,7 @@ class LoginControllerTest {
 
     @Test
     void postLogin_shouldRedirectToLoginError_whenBadCredentials() throws Exception {
-        mockMvc.perform(get("/login")
+        mockMvc.perform(post("/login")
                         .with(csrf())
                         .param("email", "bad@email.com")
                         .param("password", "wrong")
