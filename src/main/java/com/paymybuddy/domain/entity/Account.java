b281891002e3c4a26a5fd09fcb18817c13cb9a83
WebisBrian <brian.t.github@gmail.com>
2026-02-05 14:24:54 +0100

    Refacto: Add some validation

--- src/main/java/com/paymybuddy/domain/entity/Account.java
+++ src/main/java/com/paymybuddy/domain/entity/Account.java
@@ -1,10 +1,9 @@
 package com.paymybuddy.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,12 +12,14 @@ import java.math.BigDecimal;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(exclude = "user")
+@EqualsAndHashCode(of = "id")
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Balance is required.")
     @Column(nullable = false, precision=19, scale=2)
     private BigDecimal balance = BigDecimal.ZERO;
 
@@ -26,17 +27,17 @@ public class Account {
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
-    // private constructor
+    // Private constructor
     private Account(User user) {
         this.user = user;
     }
 
-    // factory method
+    // Factory method
     public static Account create(User user) {
         return new Account(user);
     }
 
-    // methods
+    // Public Methods
     public void deposit(BigDecimal amount) {
         requirePositive(amount);
         this.balance = this.balance.add(amount);
@@ -47,7 +48,7 @@ public class Account {
         this.balance = this.balance.subtract(amount);
     }
 
-    // utils
+    // Private Methods
     private static void requirePositive(BigDecimal amount) {
         if (amount == null || amount.signum() <= 0) {
             throw new IllegalArgumentException("Le montant indiqué doit être positif.");
