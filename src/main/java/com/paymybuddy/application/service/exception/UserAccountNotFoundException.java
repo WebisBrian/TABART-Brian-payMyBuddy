4ebae6f409acb73da9f06bf5fb3ba6ea398d7029
WebisBrian <brian.t.github@gmail.com>
2026-02-06 17:07:35 +0100

    Feat: overloading with email parameter

--- src/main/java/com/paymybuddy/application/service/exception/UserAccountNotFoundException.java
+++ src/main/java/com/paymybuddy/application/service/exception/UserAccountNotFoundException.java
@@ -5,4 +5,8 @@ public class UserAccountNotFoundException extends RuntimeException {
     public UserAccountNotFoundException(Long userId) {
         super("Account not found for user ID: " + userId);
     }
+
+    public UserAccountNotFoundException(String email) {
+        super("Account not found for user email: " + email);
+    }
 }
