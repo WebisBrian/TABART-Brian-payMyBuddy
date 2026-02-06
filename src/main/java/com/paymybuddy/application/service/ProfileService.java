1bb97830d4269742738d8b02cd38ae140064720a
WebisBrian <brian.t.github@gmail.com>
2026-02-06 10:22:40 +0100

    Chore: standardize methods style

--- src/main/java/com/paymybuddy/application/service/ProfileService.java
+++ src/main/java/com/paymybuddy/application/service/ProfileService.java
@@ -1,7 +1,5 @@
 package com.paymybuddy.application.service;
 
-import com.paymybuddy.domain.entity.User;
-
 public interface ProfileService {
 
     public void updateProfile(String email, String newUsername, String newEmail);
--- src/main/java/com/paymybuddy/application/service/RegistrationService.java
+++ src/main/java/com/paymybuddy/application/service/RegistrationService.java
@@ -1,5 +1,6 @@
 package com.paymybuddy.application.service;
 
 public interface RegistrationService {
+
     void register(String username, String email, String password);
 }
