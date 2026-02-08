bbb16d5d9c0e6f48a1620f817726f5a71c6d5582
WebisBrian <brian.t.github@gmail.com>
2026-02-08 17:14:57 +0100

    Feat: add new utils method for emailNormalization

--- src/main/java/com/paymybuddy/domain/utils/EmailNormalizer.java
+++ src/main/java/com/paymybuddy/domain/utils/EmailNormalizer.java
@@ -1,12 +1,17 @@
 package com.paymybuddy.domain.utils;
 
+import com.paymybuddy.domain.exception.InvalidEmailException;
+
 public class EmailNormalizer {
 
     /**
-     * Normalizes an email address by removing leading
-     * and trailing spaces and converting it to lower case.*/
+     * Normalizes an email address.
+     * The returned value is never null.
+     * The returned value is always lowercase. */
     public static String normalize(String email) {
-
+        if (email == null || email.isBlank()) {
+            throw new InvalidEmailException();
+        }
         return email.trim().toLowerCase();
     }
 }
